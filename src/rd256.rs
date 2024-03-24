fe57a710e9f366b0ceb5a983beb570097101cfff
rupam-04 <rpmdey2004@gmail.com>
2024-03-24 11:40:00 +0530

    feat: added tests for finite field arithmetic

--- src/rd256.rs
+++ src/rd256.rs
@@ -140,15 +140,52 @@ impl RD256 {
         return base;
 
     }
-
-    pub fn div_mod(&self, b: &RD256, p: &RD256) -> RD256 {
-        assert!(p.v - 2 > U256::from_big_endian(&[0]));
-        return self.mul_mod(&b.exp_mod(&RD256{ v: p.v - 2 }, p), p);
-    }
 } 
 
 impl PartialEq for RD256 {
     fn eq(&self, other: &Self) -> bool {
         return self.v == other.v;
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_mod() {
+        let a = RD256::from_str("0x1").unwrap();
+        let b = RD256::from_str("0x2").unwrap();
+        let p = RD256::from_str("0x7").unwrap();
+        let c = a.add_mod(&b, &p);
+        assert_eq!(c.v, U256::from_str("0x3").unwrap());
+    }
+
+    #[test]
+    fn test_sub_mod() {
+        let a = RD256::from_str("0x1").unwrap();
+        let b = RD256::from_str("0x2").unwrap();
+        let p = RD256::from_str("0x7").unwrap();
+        let c = a.sub_mod(&b, &p);
+        assert_eq!(c.v, U256::from_str("0x6").unwrap());
+    }
+
+    #[test]
+    fn test_mul_mod() {
+        let a = RD256::from_str("0x2").unwrap();
+        let b = RD256::from_str("0x3").unwrap();
+        let p = RD256::from_str("0x7").unwrap();
+        let c = a.mul_mod(&b, &p);
+        assert_eq!(c.v, U256::from_str("0x6").unwrap());
+    }
+
+    #[test]
+    fn test_exp_mod() {
+        let a = RD256::from_str("0x2").unwrap();
+        let b = RD256::from_str("0x3").unwrap();
+        let p = RD256::from_str("0x7").unwrap();
+        let c = a.exp_mod(&b, &p);
+        assert_eq!(c.v, U256::from_str("0x1").unwrap());
     }
 }

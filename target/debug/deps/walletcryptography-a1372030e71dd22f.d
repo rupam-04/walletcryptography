26795a906e9e0e904cb98a919daa0cb79119afd7
rupam-04 <rpmdey2004@gmail.com>
2024-03-29 02:53:44 +0530

    minor fixes

---
+++ target/debug/deps/walletcryptography-a1372030e71dd22f.d
@@ -0,0 +1,12 @@
+C:\Users\rpmde\Desktop\walletcryptography\target\debug\deps\walletcryptography-a1372030e71dd22f.exe: src\lib.rs src\rd256.rs src\bytes.rs src\secp256k1.rs src\base16.rs src\crypto.rs src\base58.rs src\app.rs
+
+C:\Users\rpmde\Desktop\walletcryptography\target\debug\deps\walletcryptography-a1372030e71dd22f.d: src\lib.rs src\rd256.rs src\bytes.rs src\secp256k1.rs src\base16.rs src\crypto.rs src\base58.rs src\app.rs
+
+src\lib.rs:
+src\rd256.rs:
+src\bytes.rs:
+src\secp256k1.rs:
+src\base16.rs:
+src\crypto.rs:
+src\base58.rs:
+src\app.rs:
---
+++ target/debug/deps/walletcryptography-d2b858e7bcabd5d3.d
@@ -0,0 +1,5 @@
+C:\Users\rpmde\Desktop\walletcryptography\target\debug\deps\walletcryptography-d2b858e7bcabd5d3.exe: src\main.rs
+
+C:\Users\rpmde\Desktop\walletcryptography\target\debug\deps\walletcryptography-d2b858e7bcabd5d3.d: src\main.rs
+
+src\main.rs:
